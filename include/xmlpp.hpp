175d0b38fbeafe6a299ae16cf5ff796bc5190f31
Michael Pingleton <mnpingleton@gmail.com>
2023-05-07 17:16:45 -0400

    Initial commit.

---
+++ include/xmlpp.hpp
@@ -0,0 +1,10 @@
+// xmlpp.hpp
+// Primary header file which joins all of the common elements of the XMLPP library.
+// Created by Michael Pingleton on 7 May 2023.
+// https://www.pinglesaur.us
+// Copyright (c) Michael Pingleton 2023
+
+#ifndef XMLPP_H
+#define XMLPP_H
+
+#endif
